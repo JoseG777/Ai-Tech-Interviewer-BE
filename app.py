65316e07719378317aac78780f774670533ecdec
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-07-19 16:09:15 -0400

    updating db for new users

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify
-import database.db_funcs as db_funcs
 from flask_cors import CORS
+from database.models import User, UserHistory
 
-# Function Imports  
+# Function Imports
 from APIs.getLeetCode import getLeetCodeInfo
 from APIs.generateProblems import generate_problem
 from APIs.evaluateResponse import evaluate_response
 
 app = Flask(__name__)
-
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 
 @app.route("/api/message", methods=["GET"])
 def get_message():
@@ -21,40 +21,54 @@ def create_user():
     data = request.get_json()
     uid = data["uid"]
     email = data["email"]
-    leetcode_username = "N/A"
+    leetcode_username = None
     user_level_description = "N/A"
- 
 
-    db_funcs.add_user(
-        uid,
-        email,
-        leetcode_username,
-        user_level_description
-    )
+    User.add_user(uid, email, leetcode_username, user_level_description)
 
     return jsonify({"message": "User created successfully"}), 201
 
 
-@app.route("/api/initialQuestions", methods=["POST"])
-def new_user_info():
-    '''
-    easy_ratio, medium_ratio, hard_ratio, overall_ratio = getLeetCodeInfo(
-        leetcode_username
-    )
-    '''
+@app.route("/api/newUser", methods=["POST"])
+def new_user():
+    data = request.get_json()
+    print(f"Recieved data: {data}")
+    uid = data["uid"]
+    leetcode_username = data["leetcode_username"]
+    coding_level = data["coding_level"]
+    goal = data["goal"]
+    upcoming_interview = data["upcoming_interview"]
 
-    pass
+    overall_ratio, easy_ratio, medium_ratio, hard_ratio = None, None, None, None
+    
+    if leetcode_username != "N/A":
+        overall_ratio, easy_ratio, medium_ratio, hard_ratio = getLeetCodeInfo(
+            leetcode_username
+        )
+        
+    print(f"Updating user: {uid}, {leetcode_username}, {coding_level}, {goal}, {upcoming_interview}, {overall_ratio}, {easy_ratio}, {medium_ratio}, {hard_ratio}")
+
+    User.update_user(
+        uid,
+        leetcode_username,
+        coding_level,
+        goal,
+        upcoming_interview,
+        overall_ratio,
+        easy_ratio,
+        medium_ratio,
+        hard_ratio,
+    )
+
+    return jsonify({"message": "New user info received"}), 201
+
 
 @app.route("/api/generateProblem", methods=["POST"])
 def generate_problem_endpoint():
     data = request.get_json()
     uid = data["uid"]
 
-    user = db_funcs.get_user_id(uid)
-
-    print(user)
-
-    print(user[0])
+    user = User.get_user_id(uid)
 
     if not user:
         return jsonify({"error": "User not found"}), 404
@@ -77,19 +91,17 @@ def evaluate_response_endpoint():
     problem = data["problem"]
     response = data["userResponse"]
     uid = data["uid"]
-    # print(uid)
 
     if problem and response and uid:
         evaluation = evaluate_response(problem, response)
-
-        # ADD FUNCTION TO SAVE TO DATABASE HERE paramaters should be (uid, problem, response, evaluation)
-        db_funcs.update_history(uid, problem, response, evaluation)
-
+        UserHistory.update_history(uid, problem, response, evaluation)
         return jsonify({"evaluation": evaluation})
 
     return jsonify({"evaluation": "error"})
 
 
 if __name__ == "__main__":
-    db_funcs.initialize_database()
+    from database.initialization import initialize_database
+
+    initialize_database()
     app.run(debug=True, port=5000)
