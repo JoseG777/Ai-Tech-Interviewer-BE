a4294cd3ad2a43a40b6dddf0f2a349e560b88ca9
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-07-22 15:00:49 -0400

    updated clearing file

--- db_manage_users.py
+++ db_manage_users.py
@@ -1,4 +1,4 @@
-# UTILITY FUNCTION TO CLEAR USERS
+# UTILITY FUNCTION TO MANAGE USERS
 
 import sqlitecloud
 import os
@@ -8,6 +8,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv('SQLITECLOUD_CONN_STRING')
 DATABASE_NAME = os.getenv('SQLITECLOUD_DB_NAME')
+
 def get_connection():
     conn = sqlitecloud.connect(DATABASE_URL)
     conn.execute(f"USE DATABASE {DATABASE_NAME}")
@@ -24,5 +25,17 @@ def clear_users_table():
     finally:
         conn.close()
 
+def delete_user_by_uid(uid):
+    conn = get_connection()
+    try:
+        conn.execute("DELETE FROM users WHERE uid = ?", (uid,))
+        conn.commit()
+        print(f"User with UID {uid} has been deleted.")
+    except Exception as e:
+        print(f"Error while deleting user with UID {uid}: {str(e)}")
+    finally:
+        conn.close()
+
 if __name__ == '__main__':
-    clear_users_table()
+    # clear_users_table() 
+    delete_user_by_uid('qeLQEOVDDHOVuY6jxnlyqdGD3bH2')  # Delete a specific user by UID
